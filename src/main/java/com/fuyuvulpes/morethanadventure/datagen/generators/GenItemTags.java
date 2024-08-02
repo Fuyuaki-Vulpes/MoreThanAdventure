137da5716d8e4740c6944b485bbb1c1e2fd6fe8e
Fuyuaki Vulpes <fuyuakivulpes@gmail.com>
2024-08-02 16:35:29 -0300

    Finished adding assets for mobs

--- src/main/java/com/fuyuvulpes/morethanadventure/datagen/generators/GenItemTags.java
+++ src/main/java/com/fuyuvulpes/morethanadventure/datagen/generators/GenItemTags.java
@@ -1,7 +1,6 @@
 package com.fuyuvulpes.morethanadventure.datagen.generators;
 
 import com.fuyuvulpes.morethanadventure.core.registry.MtaItems;
-import com.fuyuvulpes.morethanadventure.core.registry.MtaTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
