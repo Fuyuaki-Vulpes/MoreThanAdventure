add93f6c7b3341decd5a1dea916edb9357845a25
FuyuKyoshi <j-dauphinais@hotmail.com>
2024-08-10 19:17:02 -0500

    Spawn Eggs Weapons and Tags

--- src/main/java/com/fuyuvulpes/morethanadventure/datagen/generators/GenItemTags.java
+++ src/main/java/com/fuyuvulpes/morethanadventure/datagen/generators/GenItemTags.java
@@ -29,23 +29,61 @@ public class GenItemTags extends ItemTagsProvider {
                         MtaItems.COPPER_PICKAXE.get(),
                         MtaItems.COPPER_AXE.get(),
                         MtaItems.COPPER_SHOVEL.get(),
-                        MtaItems.COPPER_HOE.get()
+                        MtaItems.COPPER_HOE.get(),
+                        MtaItems.ARMAMENT_PICKAXE.get(),
+                        MtaItems.ARMAMENT_AXE.get(),
+                        MtaItems.ARMAMENT_SHOVEL.get(),
+                        MtaItems.ROYAL_PICKAXE.get(),
+                        MtaItems.ROYAL_AXE.get(),
+                        MtaItems.ROYAL_SHOVEL.get(),
+                        MtaItems.AQUATIC_PICKAXE.get(),
+                        MtaItems.AQUATIC_AXE.get(),
+                        MtaItems.AQUATIC_SHOVEL.get(),
+                        MtaItems.ANGELIC_PICKAXE.get(),
+                        MtaItems.ANGELIC_AXE.get(),
+                        MtaItems.ANGELIC_SHOVEL.get(),
+                        MtaItems.BERSERK_PICKAXE.get(),
+                        MtaItems.BERSERK_AXE.get(),
+                        MtaItems.BERSERK_SHOVEL.get(),
+                        MtaItems.FEATHERWEIGHT_PICKAXE.get(),
+                        MtaItems.FEATHERWEIGHT_AXE.get(),
+                        MtaItems.FEATHERWEIGHT_SHOVEL.get()
                 );
         this.tag(ItemTags.SWORDS)
                 .add(
-                        MtaItems.COPPER_SWORD.get()
+                        MtaItems.COPPER_SWORD.get(),
+                        MtaItems.HOLY_KNIGHTS_GREATSWORD.get(),
+                        MtaItems.SHADOW_ROGUES_DAGGER.get()
                 );
         this.tag(ItemTags.PICKAXES)
                 .add(
-                        MtaItems.COPPER_PICKAXE.get()
+                        MtaItems.COPPER_PICKAXE.get(),
+                        MtaItems.ARMAMENT_PICKAXE.get(),
+                        MtaItems.ROYAL_PICKAXE.get(),
+                        MtaItems.AQUATIC_PICKAXE.get(),
+                        MtaItems.ANGELIC_PICKAXE.get(),
+                        MtaItems.BERSERK_PICKAXE.get(),
+                        MtaItems.FEATHERWEIGHT_PICKAXE.get()
                 );
         this.tag(ItemTags.AXES)
                 .add(
-                        MtaItems.COPPER_AXE.get()
+                        MtaItems.COPPER_AXE.get(),
+                        MtaItems.ARMAMENT_AXE.get(),
+                        MtaItems.ROYAL_AXE.get(),
+                        MtaItems.AQUATIC_AXE.get(),
+                        MtaItems.ANGELIC_AXE.get(),
+                        MtaItems.BERSERK_AXE.get(),
+                        MtaItems.FEATHERWEIGHT_AXE.get()
                 );
         this.tag(ItemTags.SHOVELS)
                 .add(
-                        MtaItems.COPPER_SHOVEL.get()
+                        MtaItems.COPPER_SHOVEL.get(),
+                        MtaItems.ARMAMENT_SHOVEL.get(),
+                        MtaItems.ROYAL_SHOVEL.get(),
+                        MtaItems.AQUATIC_SHOVEL.get(),
+                        MtaItems.ANGELIC_SHOVEL.get(),
+                        MtaItems.BERSERK_SHOVEL.get(),
+                        MtaItems.FEATHERWEIGHT_SHOVEL.get()
                 );
         this.tag(ItemTags.HOES)
                 .add(
@@ -53,35 +91,62 @@ public class GenItemTags extends ItemTagsProvider {
                 );
         this.tag(ItemTags.HEAD_ARMOR)
                 .add(
-                        MtaItems.GREAT_SENTINELS_HELMET.get()
+                        MtaItems.GREAT_SENTINELS_HELMET.get(),
+                        MtaItems.HOLY_KNIGHTS_HELMET.get(),
+                        MtaItems.MYSTIC_MERMAIDS_HELMET.get(),
+                        MtaItems.TEMPLE_ANGELS_HELMET.get(),
+                        MtaItems.WRATHFUL_BERSERKERS_HELMET.get(),
+                        MtaItems.SHADOW_ROGUES_HELMET.get()
                 );
         this.tag(ItemTags.CHEST_ARMOR)
                 .add(
-                        MtaItems.GREAT_SENTINELS_CHESTPLATE.get()
+                        MtaItems.GREAT_SENTINELS_CHESTPLATE.get(),
+                        MtaItems.HOLY_KNIGHTS_CHESTPLATE.get(),
+                        MtaItems.MYSTIC_MERMAIDS_CHESTPLATE.get(),
+                        MtaItems.TEMPLE_ANGELS_CHESTPLATE.get(),
+                        MtaItems.WRATHFUL_BERSERKERS_CHESTPLATE.get(),
+                        MtaItems.SHADOW_ROGUES_CHESTPLATE.get()
                 );
         this.tag(ItemTags.LEG_ARMOR)
                 .add(
-                        MtaItems.GREAT_SENTINELS_LEGGINGS.get()
+                        MtaItems.GREAT_SENTINELS_LEGGINGS.get(),
+                        MtaItems.HOLY_KNIGHTS_LEGGINGS.get(),
+                        MtaItems.MYSTIC_MERMAIDS_LEGGINGS.get(),
+                        MtaItems.TEMPLE_ANGELS_LEGGINGS.get(),
+                        MtaItems.WRATHFUL_BERSERKERS_LEGGINGS.get(),
+                        MtaItems.SHADOW_ROGUES_LEGGINGS.get()
                 );
         this.tag(ItemTags.FOOT_ARMOR)
                 .add(
-                        MtaItems.GREAT_SENTINELS_BOOTS.get()
+                        MtaItems.GREAT_SENTINELS_BOOTS.get(),
+                        MtaItems.HOLY_KNIGHTS_BOOTS.get(),
+                        MtaItems.MYSTIC_MERMAIDS_BOOTS.get(),
+                        MtaItems.TEMPLE_ANGELS_BOOTS.get(),
+                        MtaItems.WRATHFUL_BERSERKERS_BOOTS.get(),
+                        MtaItems.SHADOW_ROGUES_BOOTS.get()
                 );/*
         this.tag(ItemTags.TRIDENT_ENCHANTABLE)
                 .add(
-                        MtaItems.GREAT_SENTINELS_BOOTS.get()
+                        MtaItems.MYSTIC_MERMAIDS_TRIDENT.get()
                 );
         this.tag(ItemTags.BOW_ENCHANTABLE)
                 .add(
-
+                        MtaItems.TEMPLE_ANGELS_BOW.get()
                 );
         this.tag(ItemTags.SWORD_ENCHANTABLE)
                 .add(
-
+                        MtaItems.COPPER_SWORD.get(),
+                        MtaItems.HOLY_KNIGHTS_GREAT_SWORD.get(),
+                        MtaItems.WRATHFUL_BERSERKERS_BATTLEAXE.get(),
+                        MtaItems.SHADOW_ROGUES_DAGGER.get()
+                );
+        this.tag(ItemTags.Items.MACE_ENCHANTABLE)
+                .add(
+                        MtaItems.GREAT_SENTINELS_HAMMER.get()
                 );
         this.tag(Tags.Items.TOOLS_BOW)
                 .add(
-
+                        MtaItems.TEMPLE_ANGELS_BOW.get();
                 );
         this.tag(Tags.Items.TOOLS_SHIELD)
                 .add(
@@ -96,6 +161,7 @@ public class GenItemTags extends ItemTagsProvider {
                 .add(
 
                 );
+
 
 */
     }
