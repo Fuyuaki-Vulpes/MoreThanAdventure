73cbaec4eb6da5aab9eaca9c3c16e3f8cf5df42d
FuyuKyoshi <j-dauphinais@hotmail.com>
2024-08-14 12:03:40 -0500

    retextured armor items

--- src/main/java/com/fuyuaki/morethanadventure/core/registry/MtaTags.java
+++ src/main/java/com/fuyuaki/morethanadventure/core/registry/MtaTags.java
@@ -46,6 +46,17 @@ public class MtaTags {
         public static final TagKey<Biome> OVERWORLD_GEYSERS = tag("overworld_geysers");
         public static final TagKey<Biome> NETHER_GEYSERS = tag("nether_geysers");
 
+        public static final TagKey<Biome> HAS_PALE_WOLF = tag("has_pale_wolf");
+        public static final TagKey<Biome> HAS_SPOTTED_WOLF = tag("has_spotted_wolf");
+        public static final TagKey<Biome> HAS_SNOWY_WOLF = tag("has_snowy_wolf");
+        public static final TagKey<Biome> HAS_BLACK_WOLF = tag("has_black_wolf");
+        public static final TagKey<Biome> HAS_ASHEN_WOLF = tag("has_ashen_wolf");
+        public static final TagKey<Biome> HAS_RUSTY_WOLF = tag("has_rusty_wolf");
+        public static final TagKey<Biome> HAS_WOODS_WOLF = tag("has_woods_wolf");
+        public static final TagKey<Biome> HAS_CHESTNUT_WOLF = tag("has_chestnut_wolf");
+        public static final TagKey<Biome> HAS_STRIPED_WOLF = tag("has_striped_wolf");
+    }
+
 
         private static TagKey<Biome> tag(String name) {
             return TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(MODID, name));
