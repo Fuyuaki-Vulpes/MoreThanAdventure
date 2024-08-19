c7e88c8ec9bbf5b831d6ac46b2bac102d04de7a8
Fuyuaki Vulpes <fuyuakivulpes@gmail.com>
2024-08-19 18:58:47 -0300

    ShardGrowthBlock & Stripped Palm

--- src/main/java/com/fuyuaki/morethanadventure/world/block/ShardGrowthBlock.java
+++ src/main/java/com/fuyuaki/morethanadventure/world/block/ShardGrowthBlock.java
@@ -4,10 +4,12 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.AmethystClusterBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.List;
@@ -15,33 +17,35 @@ import java.util.List;
 public class ShardGrowthBlock extends Block {
     private final Block replacement;
     private final ShardClusterBlock shardClusterBlock;
+    private static final Direction[] DIRECTIONS = Direction.values();
 
     public ShardGrowthBlock(Block replacement, ShardClusterBlock shardClusterBlock, Properties p_49795_) {
-        super(p_49795_);
+        super(p_49795_.randomTicks());
         this.replacement = replacement;
         this.shardClusterBlock = shardClusterBlock;
     }
 
-    @Override
-    protected void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
-        if (pRandom.nextBoolean()){
-            List<BlockPos> posList = List.of(pPos.above(),pPos.below(),pPos.east(),pPos.north(),pPos.west(),pPos.south());
-            for (BlockPos pos : posList){
-                if (pRandom.nextFloat() < 0.4F && (pLevel.getBlockState(pos).is(Blocks.WATER) || pLevel.getBlockState(pos).isAir())){
-                    pLevel.setBlock(pos,shardClusterBlock.defaultBlockState()
-                            .setValue(BlockStateProperties.WATERLOGGED, pLevel.getBlockState(pos).is(Blocks.WATER))
-                                    .setValue(BlockStateProperties.FACING,
-                                            Direction.getNearest(
-                                                    new Vec3(
-                                                            pos.getCenter().subtract(pPos.getCenter()).toVector3f()
-                                                    ))),
 
-                            3);
-                }
+    @Override
+    protected void randomTick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
+        if (pRandom.nextInt(5) == 0) {
+            Direction direction = DIRECTIONS[pRandom.nextInt(DIRECTIONS.length)];
+            BlockPos blockpos = pPos.relative(direction);
+            BlockState blockstate = pLevel.getBlockState(blockpos);
+            Block block = null;
+            if (canClusterGrowAtState(blockstate)) {
+                block = shardClusterBlock;
+            }
+
+            if (block != null) {
+                BlockState blockstate1 = block.defaultBlockState()
+                        .setValue(ShardClusterBlock.FACING, direction)
+                        .setValue(ShardClusterBlock.WATERLOGGED, Boolean.valueOf(blockstate.getFluidState().getType() == Fluids.WATER));
+                pLevel.setBlockAndUpdate(blockpos, blockstate1);
             }
         }
-        if (pRandom.nextFloat() < 0.05F){
-            pLevel.setBlock(pPos,replacement.defaultBlockState(),3);
-        }
+    }
+    public static boolean canClusterGrowAtState(BlockState pState) {
+        return pState.isAir() || pState.is(Blocks.WATER) && pState.getFluidState().getAmount() == 8;
     }
 }
