9807e70b372eca69f3ef8bb0a22043db4c1e9d35
FuyuKyoshi <j-dauphinais@hotmail.com>
2024-12-13 14:09:18 -0600

    Monster Hitbox size adjusted
    yey

--- src/main/java/com/fuyuaki/morethanadventure/core/registry/MtaEntityTypes.java
+++ src/main/java/com/fuyuaki/morethanadventure/core/registry/MtaEntityTypes.java
@@ -33,64 +33,64 @@ public class MtaEntityTypes {
 
     public static final DeferredHolder<EntityType<?>,EntityType<ArmoredSkeleton>> ARMORED_SKELETON = ENTITY.register("armored_skeleton",
             () -> EntityType.Builder.<ArmoredSkeleton>of(ArmoredSkeleton::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.675F, 2.1F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"armored_skeleton").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<BlackWidow>> BLACK_WIDOW = ENTITY.register("black_widow",
             () -> EntityType.Builder.<BlackWidow>of(BlackWidow::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(1.55F, 0.725F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"black_widow").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<CharredSkeleton>> CHARRED_SKELETON = ENTITY.register("charred_skeleton",
             () -> EntityType.Builder.<CharredSkeleton>of(CharredSkeleton::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.675F, 2.1F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"charred_skeleton").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<CorrosiveCube>> CORROSIVE_CUBE = ENTITY.register("corrosive_cube",
             () -> EntityType.Builder.<CorrosiveCube>of(CorrosiveCube::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.55F, 0.55F)
                     .spawnDimensionsScale(4.0F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"corrosive_cube").toString()));
     public static final DeferredHolder<EntityType<?>,EntityType<FrostedSlime>> FROSTED_SLIME = ENTITY.register("frosted_slime",
             () -> EntityType.Builder.<FrostedSlime>of(FrostedSlime::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.55F, 0.55F)
                     .spawnDimensionsScale(4.0F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"frosted_slime").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<GlowSpider>> GLOW_SPIDER = ENTITY.register("glow_spider",
             () -> EntityType.Builder.<GlowSpider>of(GlowSpider::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.725F, 0.40F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"glow_spider").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<Host>> HOST = ENTITY.register("host",
             () -> EntityType.Builder.<Host>of(Host::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.85F, 2.05F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"host").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<MossyZombie>> MOSSY_ZOMBIE = ENTITY.register("mossy_zombie",
             () -> EntityType.Builder.<MossyZombie>of(MossyZombie::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.85F, 2.05F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"mossy_zombie").toString()));
 
 
     public static final DeferredHolder<EntityType<?>,EntityType<SkeletonFighter>> SKELETON_FIGHTER = ENTITY.register("skeleton_fighter",
             () -> EntityType.Builder.<SkeletonFighter>of(SkeletonFighter::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.675F, 2.1F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"skeleton_fighter").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<ToxicZombie>> TOXIC_ZOMBIE = ENTITY.register("toxic_zombie",
             () -> EntityType.Builder.<ToxicZombie>of(ToxicZombie::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.85F, 2.05F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"toxic_zombie").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<ZombifiedMiner>> ZOMBIFIED_MINER = ENTITY.register("zombified_miner",
             () -> EntityType.Builder.<ZombifiedMiner>of(ZombifiedMiner::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.85F, 2.05F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"zombified_miner").toString()));
 
     public static final DeferredHolder<EntityType<?>,EntityType<Siren>> SIREN = ENTITY.register("siren",
             () -> EntityType.Builder.<Siren>of(Siren::new, MobCategory.MONSTER)
-                    .sized(0.6F, 1.7F)
+                    .sized(0.85F, 2.0F)
                     .build(ResourceLocation.fromNamespaceAndPath(MODID,"siren").toString()));
 
 
